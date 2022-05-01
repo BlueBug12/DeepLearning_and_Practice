fixed data scaling bug (already divided by 255.)

Lab5/dataset.py
@@ -53,7 +53,7 @@ class bair_robot_pushing_dataset(Dataset):
             im = np.asarray(im).reshape(64, 64, 3)
             im = self.to_tensor(im)
             im = im.reshape(1,3,64,64)
-            image_seq.append(im/255.)
+            image_seq.append(im)
         image_seq = np.concatenate(image_seq, axis=0)
         return image_seq
     

Lab5/train_fixed_prior.py
@@ -23,7 +23,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--lr', default=0.002, type=float, help='learning rate')
     parser.add_argument('--beta1', default=0.9, type=float, help='momentum term for adam')
-    parser.add_argument('--batch_size', default=12, type=int, help='batch size')
+    parser.add_argument('--batch_size', default=32, type=int, help='batch size')
     parser.add_argument('--log_dir', default='./logs/fp', help='base directory to save logs')
     parser.add_argument('--model_dir', default='', help='base directory to save logs')
     parser.add_argument('--data_root', default='./data', help='root directory for data')
@@ -81,14 +81,8 @@ def train(x, cond, modules, optimizer, kl_anneal, args):
         _, mu_p, logvar_p = modules['prior'](h)
         h_pred = modules['frame_predictor'](torch.cat([h, z_t], 1))
         x_pred = modules['decoder']([h_pred, skip],cond[i])
-        #print(type(x_pred))
-        #print(x_pred.shape)
-        #print(type(x[i]))
-        #print(x[i].shape)
-        #exit(1)
         mse += mse_criterion(x_pred, x[i])
         kld += kl_criterion(mu, logvar, mu_p, logvar_p,args)
-        #print("test")
 
     #beta = kl_anneal.get_beta()
     beta = 0.87
@@ -313,8 +307,8 @@ def main():
             train_record.write(('[epoch: %02d] loss: %.5f | mse loss: %.5f | kld loss: %.5f\n' % (epoch, epoch_loss  / args.epoch_size, epoch_mse / args.epoch_size, epoch_kld / args.epoch_size)))
         
         frame_predictor.eval()
-        encoder.eval()
-        decoder.eval()
+        #encoder.eval()
+        #decoder.eval()
         prior.eval()
         posterior.eval()
 
@@ -350,7 +344,7 @@ def main():
                     'last_epoch': epoch},
                     '%s/model.pth' % args.log_dir)
 
-        if epoch % 2 == 0:
+        if epoch % 10 == 0:
             try:
                 validate_seq, validate_cond = next(validate_iter)
             except StopIteration:
@@ -358,7 +352,7 @@ def main():
                 validate_seq, validate_cond = next(validate_iter)
 
             plot_pred(validate_seq, validate_cond, modules, epoch, args)
-            plot_rec(validate_seq, validate_cond, modules, epoch, args)
+            #plot_rec(validate_seq, validate_cond, modules, epoch, args)
 
 if __name__ == '__main__':
     main()

Lab5/utils.py
@@ -256,14 +256,11 @@ def plot_pred(validate_seq, validate_cond, modules, epoch, args):
     #fname = '%s/gen/sample_%d.png' % (args.log_dir, epoch) 
     #save_tensors_image(fname, to_plot)
 
-    pred_fname = '%s/gen/sample_%d_pred.gif' % (args.log_dir, epoch) 
-    gt_fname = '%s/gen/sample_%d_gt.gif' % (args.log_dir, epoch) 
-    save_gif(pred_fname, gt_fname, gifs)
+    pred_fname = '%s/gen/sample_%d_all_pred.gif' % (args.log_dir, epoch) 
+    #gt_fname = '%s/gen/sample_%d_gt.gif' % (args.log_dir, epoch) 
+    save_gif(pred_fname, gifs)
 
 def save_tensors_image(filename, inputs, padding=1):
-    print(len(inputs))
-    print(len(inputs[0]))
-    print(inputs[0][0].shape)
     images = image_tensor(inputs, padding)
     return save_image(filename, images)
 
@@ -304,121 +301,31 @@ def plot_rec(validate_seq, validate_cond, modules, epoch, args ):#validate_seq, 
     #fname = '%s/gen/rec_%d.png' % (args.log_dir, epoch) 
     #save_tensors_image(fname, to_plot)
 
+def tensor_to_img(input):
+    img = input.detach().cpu().transpose(0,1).transpose(1,2).clamp(0,1).numpy()
+    img = img / np.amax(img)
+    img = 255*img
+    img = img.astype(np.uint8)
+    return img
 
-def save_gif(pred_fname, gt_fname, inputs, duration=0.25):
+def save_gif(pred_fname, inputs, duration=0.5):
     #inputs shape:(seq, batch, 6, 3, 64, 64)
 
-    images = []
-    preds = []
+    num = len(inputs[0][0])
+    gifs = [ [] for t in range(num) ]
     for i in range(len(inputs)):
-        img = inputs[i][0][0]
-        pre = inputs[i][0][1]
-        img = img.cpu()
-        pre = pre.cpu()
-        img = img.transpose(0,1).transpose(1,2).clamp(0,1).numpy()
-        pre = pre.transpose(0,1).transpose(1,2).clamp(0,1).numpy()
-        img = img / np.amax(img)
-        pre = pre / np.amax(pre)
-  
-        img = 255*img
-        pre = 255*pre
+        for j in range(num):
+            gifs[j].append(tensor_to_img(inputs[i][0][j]))
 
-        img = img.astype(np.uint8)
-        pre = pre.astype(np.uint8)
-
-        images.append(img)
-        preds.append(pre)
     #imageio.mimsave(gt_fname, images, duration=duration)
     #images_future = images[-5:]
         #preds.append(img.numpy().astype('uint8'))
-    
-    t1 = []
-    t2 = []
+    '''
     for i in range(len(inputs)):
-        t1.append(np.concatenate([images[i],preds[i]],axis=0))
-        t2.append(np.concatenate([images[i],preds[i]],axis=1))
-    imageio.mimsave(pred_fname, t2, duration=duration)
-    #imageio.mimsave(pred_fname, t1[-5:], duration=duration)
-    
-
-    
-    
+        img = gifs[0][i]
+        for j in range(1,num):
+            img = np.concatenate([img,gifs[j][i]],axis=1)
+        con_gif.append(img)
     '''
-    for tensor in inputs:
-        img = image_tensor(tensor, padding=0)
-        print(img.shape)
-        print(img)
-        img = img.transpose(0,1).transpose(1,2).clamp(0,1)
-        images.append(img.numpy()*255)
-        exit(1)
-        img = img.cpu()
-        img = img.transpose(0,1).transpose(1,2).clamp(0,1)
-        images.append(img.numpy()*255)
-    imageio.mimsave(gt_fname, images, duration=duration)
-    
-    preds = []
-    for tensor in inputs[0]:
-        img = image_tensor(tensor, padding=0)
-        img = img.cpu()
-        img = img.transpose(0,1).transpose(1,2).clamp(0,1)
-        preds.append(img.numpy()*255)
-    imageio.mimsave(pred_fname, preds, duration=duration)
-    '''
-    
-'''
-def plot(validate_seq, validate_cond, modules, epoch, args):
-    nsample = 5 
-    gen_seq = [[] for i in range(nsample)]
-    gt_seq = [validate_seq[i] for i in range(len(validate_seq))]
-
-    h_seq = [modules['encoder'](validate_seq[i],validate_cond[i]) for i in range(args.n_past)]
-    for s in range(nsample):
-        modules['frame_predictor'].hidden = modules['frame_predictor'].init_hidden()
-        gen_seq[s].append(validate_seq[0])
-        x_in = validate_seq[0]
-        for i in range(1, args.n_eval):
-            if args.last_frame_skip or i < args.n_past:	
-                h, skip = h_seq[i-1]
-                h = h.detach()
-            elif i < args.n_past:
-                h, _ = h_seq[i-1]
-                h = h.detach()
-            if i < args.n_past:
-                z_t, _, _ = modules['posterior'](h_seq[i][0])
-                modules['frame_predictor'](torch.cat([h, z_t], 1)) 
-                x_in = validate_seq[i]
-                gen_seq[s].append(x_in)
-            else:
-                z_t = torch.cuda.FloatTensor(args.batch_size, args.z_dim).normal_()
-                h = modules['frame_predictor'](torch.cat([h, z_t], 1)).detach()
-                x_in = modules['decoder']([h, skip],validate_cond[i]).detach()
-                gen_seq[s].append(x_in)
-
-    to_plot = []
-    gifs = [ [] for t in range(args.n_eval) ]
-    nrow = min(args.batch_size, 10)
-    for i in range(nrow):
-        # ground truth sequence
-        row = [] 
-        for t in range(args.n_eval):
-            row.append(gt_seq[t][i])
-        to_plot.append(row)
-
-        for s in range(nsample):
-            row = []
-            for t in range(args.n_eval):
-                row.append(gen_seq[s][t][i]) 
-            to_plot.append(row)
-        for t in range(args.n_eval):
-            row = []
-            row.append(gt_seq[t][i])
-            for s in range(nsample):
-                row.append(gen_seq[s][t][i])
-            gifs[t].append(row)
-
-    fname = '%s/gen/sample_%d.png' % (args.log_dir, epoch) 
-    save_tensors_image(fname, to_plot)
-
-    fname = '%s/gen/sample_%d.gif' % (args.log_dir, epoch) 
-    save_gif(fname, gifs)
-'''
+    gifs = np.concatenate(gifs, axis=0)
+    imageio.mimsave(pred_fname, gifs, duration=duration)